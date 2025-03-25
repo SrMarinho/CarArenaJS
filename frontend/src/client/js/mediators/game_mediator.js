fb82645878e276c86b23eb2eb430aede2e11b08f
Matheus Marinho <temarinho76@gmail.com>
2025-03-25 08:50:53 -0300

    update: improve mediator

--- frontend/src/client/js/mediators/game_mediator.js
+++ frontend/src/client/js/mediators/game_mediator.js
@@ -1,18 +1,20 @@
-import { UIStates } from "../data/uiState"
 import { GameState } from "../data/gameState"
 
 class GameMediator {
     constructor(game, ui) {
         this.game = game
         this.ui = ui
+        this.setup()
+    }
 
+    setup() {
         this.game.setMediator(this)
         this.ui.setMediator(this)
     }
 
     notify(sender, event, data) {
         switch (event) {
-            case UIStates.MAIN_MENU:
+            case GameState.MAIN_MENU:
                 this.game.status = GameState.MAIN_MENU
                 break;
             default:
